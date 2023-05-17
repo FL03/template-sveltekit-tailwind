0e22da2c20492966c6504e7edf11afe5dbc9155d
FL03 <jo3mccain@icloud.com>
2023-05-16 22:02:26 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ src/routes/feed/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PostTimeline from '$lib/cmp/posts/PostTimeline.svelte';
+
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
+
+<svelte:head>
+  <title>Feed</title>
+</svelte:head>
+
+<PostTimeline articles={data.articles} />
+
+<style>
+</style>
