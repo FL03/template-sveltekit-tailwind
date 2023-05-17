56f482c32dda86a1173d38770228a7e038f57aba
FL03 <jo3mccain@icloud.com>
2023-05-17 15:18:52 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/lib/firebase/admin/admin.server.ts
+++ src/lib/firebase/admin/admin.server.ts
@@ -1,8 +1,10 @@
 import { dev } from '$app/environment';
-import { initializeApp } from 'firebase-admin/app';
+import { initializeApp, type AppOptions } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { devConfig, firebaseConfig } from '$lib/firebase/firebase.config.js';
+
+
+let firebaseConfig: AppOptions;
 
 if (dev || process.env.NODE_ENV === 'development') {
   process.env['FIREBASE_AUTH_EMULATOR_HOST'] = '127.0.0.1:9099';
@@ -10,10 +12,19 @@ if (dev || process.env.NODE_ENV === 'development') {
 }
 
 if (dev) {
-  console.log('firebaseConfig (server)', devConfig);
+  firebaseConfig = {
+    projectId: "svkcl-d7eb1",
+    storageBucket: "demo.appspot.com"
+  };
+  console.log('firebaseConfig (server)', firebaseConfig);
+} else {
+  firebaseConfig = {
+    projectId: "svkcl-d7eb1",
+    storageBucket: "svkcl-d7eb1.appspot.com"
+  }
 }
 
 // this is the server-side firebase client
-export const app = initializeApp(dev ? devConfig : firebaseConfig, 'server' + Math.random());
+export const app = initializeApp(firebaseConfig, 'server' + Math.random());
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
