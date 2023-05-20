250164c8017aae9a330acaae7651957630e03953
FL03 <jo3mccain@icloud.com>
2023-05-20 08:36:33 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/routes/profile/@[user]/+layout.server.ts
+++ src/routes/profile/@[user]/+layout.server.ts
@@ -1,7 +1,10 @@
+import { firestore } from "$lib/firebase/admin/admin.server";
+
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ params }) {
-  // const doc = await firestore.collection('users').doc(`${params.user}`).get();
+  const doc = await firestore.collection('users').doc(`${params.user}`).get();
   return {
-    params
+    params,
+    profile: { ...doc.data()}
   };
 }
--- src/routes/profile/@[user]/+layout.svelte
+++ src/routes/profile/@[user]/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script>
   import { onMount } from 'svelte';
   import { enhance } from '$app/forms';
   import { page } from '$app/stores';
@@ -12,101 +12,97 @@
     DropdownItem,
     MenuButton
   } from 'flowbite-svelte';
-  import { firestore } from '$lib/firebase/stores';
 
   // Bindings
   let horizontal = false;
-  // Values
-  let profile: import('$lib/types/users').User | undefined;
 
   /** @type {import('./$types').PageData} */
   export let data;
 
   $: is_favorites = $page.route.id === '/profile/@[user]/favorites';
+  $: profile = data.profile;
   $: user = $page.data.session.user;
 
   onMount(async () => {
-    const pdoc = await firestore.getDocument(`users/${$page.params.user}`);
-    profile = { ...pdoc.data(), email: pdoc.data()?.email, uid: pdoc.id, name: pdoc.data()?.name };
+
   });
 </script>
 
 <svelte:head>
-  <title>{profile?.username || profile?.name} • Profile</title>
+  <title>{profile.username || profile.name} • Profile</title>
 </svelte:head>
 
-{#if profile}
-  <Card {horizontal}>
-    <div class="flex justify-end">
-      <MenuButton />
-      <Dropdown class="w-36">
-        <DropdownItem>Edit</DropdownItem>
-        <DropdownItem>Export data</DropdownItem>
-        <DropdownItem>Delete</DropdownItem>
-      </Dropdown>
-    </div>
-    <div class="flex flex-col items-center pb-4">
-      <Avatar size="lg" src={profile?.picture} />
-      <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{profile.name}</h5>
-      {#if profile.bio}
-        <span class="text-sm text-gray-500 dark:text-gray-400">{profile.bio}</span>
-      {/if}
-
-      {#if profile.uid === user?.uid}
-        <a href="/settings" class="btn btn-sm btn-outline-secondary action-btn">
-          <i class="ion-gear-a" />
-          Settings
-        </a>
-      {:else if user}
-        <form
-          method="POST"
-          action="/profile/@{profile.uid}?/toggleFollow"
-          use:enhance={({ form }) => {
-            // optimistic UI
-            profile.following = !profile.following;
-
-            const button = form.querySelector('button');
-            button.disabled = true;
-
-            return ({ result, update }) => {
-              button.disabled = false;
-              if (result.type === 'error') update();
-            };
-          }}
-        >
-          <input hidden type="checkbox" name="following" checked={profile.following} />
-          <Button>
-            <i class="ion-plus-round" />
-            {profile.following ? 'Unfollow' : 'Follow'}
-            {profile.username}
-          </Button>
-        </form>
-        <div class="flex mt-4 space-x-3 lg:mt-6">
-          <Button>Add friend</Button>
-          <Button color="light" class="dark:text-white">Message</Button>
-        </div>
-      {:else}
-        <a href="/login">Sign in to follow</a>
-      {/if}
-    </div>
-  </Card>
-
-  <div class="articles-toggle">
-    <ul class="nav nav-pills outline-active">
-      <li class="nav-item">
-        <a href="/profile/@{profile.uid}" class="nav-link" class:active={!is_favorites}>
-          Articles
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a href="/profile/@{profile.uid}/favorites" class="nav-link" class:active={is_favorites}>
-          Favorites
-        </a>
-      </li>
-    </ul>
+<Card {horizontal}>
+  <div class="flex justify-end">
+    <MenuButton />
+    <Dropdown class="w-36">
+      <DropdownItem>Edit</DropdownItem>
+      <DropdownItem>Export data</DropdownItem>
+      <DropdownItem>Delete</DropdownItem>
+    </Dropdown>
   </div>
-{/if}
+  <div class="flex flex-col items-center pb-4">
+    <Avatar size="lg" src={profile.picture} />
+    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{profile.name}</h5>
+    {#if profile.bio}
+      <span class="text-sm text-gray-500 dark:text-gray-400">{profile.bio}</span>
+    {/if}
+
+    {#if profile.uid === user?.uid}
+      <a href="/settings" class="btn btn-sm btn-outline-secondary action-btn">
+        <i class="ion-gear-a" />
+        Settings
+      </a>
+    {:else if user}
+      <form
+        method="POST"
+        action="/profile/@{profile.uid}?/toggleFollow"
+        use:enhance={({ form }) => {
+          // optimistic UI
+          profile.following = !profile.following;
+
+          const button = form.querySelector('button');
+          button.disabled = true;
+
+          return ({ result, update }) => {
+            button.disabled = false;
+            if (result.type === 'error') update();
+          };
+        }}
+      >
+        <input hidden type="checkbox" name="following" checked={profile.following} />
+        <Button>
+          <i class="ion-plus-round" />
+          {profile.following ? 'Unfollow' : 'Follow'}
+          {profile.username}
+        </Button>
+      </form>
+      <div class="flex mt-4 space-x-3 lg:mt-6">
+        <Button>Add friend</Button>
+        <Button color="light" class="dark:text-white">Message</Button>
+      </div>
+    {:else}
+      <a href="/login">Sign in to follow</a>
+    {/if}
+  </div>
+</Card>
+
+<div class="articles-toggle">
+  <ul class="nav nav-pills outline-active">
+    <li class="nav-item">
+      <a href="/profile/@{profile.uid}" class="nav-link" class:active={!is_favorites}>
+        Articles
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href="/profile/@{profile.uid}/favorites" class="nav-link" class:active={is_favorites}>
+        Favorites
+      </a>
+    </li>
+  </ul>
+</div>
+
 
 <section class="py-2 w-full items-center justify-center">
   <slot />
