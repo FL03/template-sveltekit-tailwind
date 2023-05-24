4755093614649bc3962e440da4a46de31e1217da
FL03 <jo3mccain@icloud.com>
2023-05-23 23:44:12 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,6 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::api::shell;
 use tauri::{
   AboutMetadata, CustomMenuItem, Manager, Menu, MenuEntry, MenuItem, Submenu, WindowBuilder,
   WindowUrl,
